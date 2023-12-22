52426f1d2b1362acccf8d0cff0c14bfd2e87c599
Mistress Plague <krewellamusicps3@gmail.com>
2023-12-22 02:44:57 +0000

    Try to automatically resolve mod name and versions when download link is inputted first Make downloaded files in install process use original file names Name and Version fields now are visual only due to the above

--- Easy Minecraft Modpacks/MainUI.cs
+++ Easy Minecraft Modpacks/MainUI.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Mime;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -31,6 +33,8 @@ namespace Easy_Minecraft_Modpacks
 
         public static ConfigLib<Configuration> Config;
 
+        private static WebClient client = new WebClient();
+
         public MainUI()
         {
             InitializeComponent();
@@ -42,14 +46,14 @@ namespace Easy_Minecraft_Modpacks
             {
                 return;
             }
-            
+
             using var popup = new OpenFileDialog();
             popup.Filter = "Modpack Config|*.Modpack.json";
 
             if (popup.ShowDialog() == DialogResult.OK)
             {
                 Config = new ConfigLib<Configuration>(popup.FileName);
-                
+
                 dataGridView1.Rows.Clear();
 
                 foreach (var mod in Config.InternalConfig.Mods)
@@ -70,7 +74,7 @@ namespace Easy_Minecraft_Modpacks
                     MessageBox.Show("Please fix the errors in formatting before saving.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     return;
                 }
-                
+
                 if (string.IsNullOrWhiteSpace(row.Cells["Version"]?.Value?.ToString()) || string.IsNullOrWhiteSpace(row.Cells["ModName"]?.Value?.ToString()) || string.IsNullOrWhiteSpace(row.Cells["Download"]?.Value?.ToString()))
                 {
                     continue;
@@ -162,9 +166,23 @@ namespace Easy_Minecraft_Modpacks
                                 cell.Style.BackColor = dataGridView1.DefaultCellStyle.BackColor;
                                 cell.Style.SelectionBackColor = dataGridView1.DefaultCellStyle.SelectionBackColor;
 
-                                if (value.Contains("cdn.modrinth.com"))
+                                if (string.IsNullOrEmpty(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString()) && string.IsNullOrEmpty(dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString()))
                                 {
-                                    // ToDo
+                                    var filename = client.GetFileName(value);
+
+                                    var name = Regex.Match(filename, @"[a-zA-Z _\-]*").Value;
+                                
+                                    var modName = (!string.IsNullOrWhiteSpace(name) ? name : "").Replace("-", " ").Trim();
+                                
+                                    modName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(modName).Replace("Api", "API").Replace("Fabric", "").Replace("Forge", "").Trim();
+
+                                    var verMatches = Regex.Matches(filename, @"\d+\.\d+(\.\d)?");
+                                    var ver = verMatches[verMatches.Count - 1].Value;
+
+                                    var modVersion = dataGridView1.Rows.Count > 1 && e.RowIndex > 0 ? dataGridView1.Rows[e.RowIndex - 1].Cells[0].Value?.ToString() ?? ver : (!string.IsNullOrWhiteSpace(ver) ? ver : "");
+
+                                    dataGridView1.Rows[e.RowIndex].Cells[0].Value = modVersion;
+                                    dataGridView1.Rows[e.RowIndex].Cells[1].Value = modName;
                                 }
                             }
 
@@ -178,22 +196,22 @@ namespace Easy_Minecraft_Modpacks
         private void installToolStripMenuItem_Click(object sender, EventArgs e)
         {
             installToolStripMenuItem.Enabled = false;
-            
+
             MessageBox.Show("Please select your Minecraft directory.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            
+
             using var folderDialog = new FolderBrowserDialog();
             folderDialog.Description = "Select your Minecraft directory";
 
-            var mcdir =$"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\\.minecraft";
-            
+            var mcdir = $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\\.minecraft";
+
             folderDialog.SelectedPath = Directory.Exists(mcdir) ? mcdir : Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-            
+
             if (folderDialog.ShowDialog() == DialogResult.OK)
             {
                 var minecraftDirectory = folderDialog.SelectedPath;
-                
+
                 var modsFolder = Path.Combine(minecraftDirectory, "mods");
-                
+
                 if (Directory.Exists(modsFolder))
                 {
                     if (File.Exists($"{modsFolder}\\dont_backup"))
@@ -204,33 +222,28 @@ namespace Easy_Minecraft_Modpacks
                     {
                         RetryNaming:
                         var backupFolder = Path.Combine(minecraftDirectory, "mods_backup") + "_" + Guid.NewGuid().ToString("N");
-                    
+
                         if (Directory.Exists(backupFolder))
                         {
                             goto RetryNaming;
                         }
-                    
+
                         Directory.Move(modsFolder, backupFolder);
                     }
                 }
-                
+
                 Directory.CreateDirectory(modsFolder);
-                
+
                 foreach (var mod in Config.InternalConfig.Mods)
                 {
-                    var modFileName = $"{mod.Name}-{mod.Version}.jar";
-                    var modFilePath = Path.Combine(modsFolder, modFileName);
-
-                    using var client = new WebClient();
-                    
-                    client.DownloadFile(mod.DownloadLink, modFilePath);
+                    client.BetterDownloadFile(mod.DownloadLink, modsFolder);
                 }
-                
+
                 File.WriteAllText($"{modsFolder}\\dont_backup", "");
 
                 MessageBox.Show("Done!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
-            
+
             installToolStripMenuItem.Enabled = true;
         }
 
@@ -240,43 +253,41 @@ namespace Easy_Minecraft_Modpacks
             {
                 return;
             }
-            
+
             MessageBox.Show("Please select your Minecraft directory.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            
+
             using var folderDialog = new FolderBrowserDialog();
             folderDialog.Description = "Select your Minecraft directory";
 
-            var mcdir =$"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\\.minecraft";
-            
+            var mcdir = $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\\.minecraft";
+
             folderDialog.SelectedPath = Directory.Exists(mcdir) ? mcdir : Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
 
             if (folderDialog.ShowDialog() == DialogResult.OK)
             {
                 Enabled = false;
-                
+
                 var minecraftDirectory = folderDialog.SelectedPath;
 
                 var modsFolder = Path.Combine(minecraftDirectory, "mods");
 
                 var mods = Directory.GetFiles(modsFolder, "*.jar", SearchOption.TopDirectoryOnly);
-                
+
                 dataGridView1.Rows.Clear();
-                
+
                 foreach (var modFile in mods)
                 {
                     var uploadedFile = await GoFileAPI.UploadFile(modFile);
-                    
+
                     var modFileName = Path.GetFileNameWithoutExtension(modFile);
-                    
-                    // This does not account for the file not having a dash
+
                     var name = Regex.Match(modFileName, @"[a-zA-Z _]*").Value;
                     var modName = !string.IsNullOrWhiteSpace(name) ? name : modFileName;
 
                     var ver = Regex.Match(modFileName, @"\d+\.\d+(\.\d)?").Value;
-                    
+
                     var modVersion = !string.IsNullOrWhiteSpace(ver) ? ver : "";
-                    
-                    // add to datagridview
+
                     dataGridView1.Rows.Add(modVersion, modName, uploadedFile.Item2);
                 }
 
@@ -294,7 +305,7 @@ namespace Easy_Minecraft_Modpacks
                 {
                     currmods.Add(new ModInfo());
                 }
-                
+
                 if (string.IsNullOrWhiteSpace(row.Cells["Version"]?.Value?.ToString()) || string.IsNullOrWhiteSpace(row.Cells["ModName"]?.Value?.ToString()) || string.IsNullOrWhiteSpace(row.Cells["Download"]?.Value?.ToString()))
                 {
                     continue;
@@ -325,7 +336,7 @@ namespace Easy_Minecraft_Modpacks
                 {
                     mods.Add(new ModInfo());
                 }
-                
+
                 if (string.IsNullOrWhiteSpace(row.Cells["Version"]?.Value?.ToString()) || string.IsNullOrWhiteSpace(row.Cells["ModName"]?.Value?.ToString()) || string.IsNullOrWhiteSpace(row.Cells["Download"]?.Value?.ToString()))
                 {
                     continue;
@@ -342,6 +353,103 @@ namespace Easy_Minecraft_Modpacks
                     e.Cancel = true;
                 }
             }
+        }
+    }
+
+    public static class Extensions
+    {
+        /// <summary>
+        /// Downloads a file from the specified URL and saves it to the target directory.
+        /// </summary>
+        /// <param name="client">The WebClient instance used to download the file.</param>
+        /// <param name="url">The URL of the file to download.</param>
+        /// <param name="targetDir">The target directory where the downloaded file will be saved.</param>
+        /// <returns>The filename of the downloaded file.</returns>
+        public static string BetterDownloadFile(this WebClient client, string url, string targetDir)
+        {
+            var data = client.DownloadData(url);
+
+            var disposition = client.ResponseHeaders["Content-Disposition"];
+            
+            var filename = disposition != null ? new ContentDisposition(disposition).FileName : url.Substring(url.LastIndexOf("/", StringComparison.Ordinal) + 1);
+
+            File.WriteAllBytes($"{targetDir}\\{filename}", data);
+
+            return filename;
+        }
+
+        /// <summary>
+        /// Retrieves the filename from the response headers of the given URL.
+        /// </summary>
+        /// <param name="client">The WebClient instance used to make the request.</param>
+        /// <param name="url">The URL from which to extract the filename.</param>
+        /// <returns>
+        /// The filename extracted from the response headers of the given URL.
+        /// </returns>
+        public static string GetFileName(this WebClient client, string url)
+        {
+            client.DownloadData(url);
+
+            var redir = Workarounds.GetRedirectedUrl(url);
+
+            var disposition = client.ResponseHeaders["Content-Disposition"];
+            
+            var locationEnding = redir?.Substring(redir.LastIndexOf("/", StringComparison.Ordinal) + 1);
+            
+            var hasQuery = locationEnding?.IndexOf("?", StringComparison.Ordinal) ?? -1;
+
+            if (hasQuery != -1)
+            {
+                locationEnding = locationEnding?.Substring(0, hasQuery);
+            }
+                
+            var urlEnding = url.Substring(url.LastIndexOf("/", StringComparison.Ordinal) + 1);
+            
+            var filename = disposition != null ? new ContentDisposition(disposition).FileName : (locationEnding != null ? locationEnding : urlEnding);
+
+            return filename;
+        }
+    }
+
+    public class Workarounds
+    {
+        public static string GetRedirectedUrl(string url)
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)HttpWebRequest.Create(url);
+            webRequest.AllowAutoRedirect = false;
+
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse())
+                {
+                    if (response.StatusCode == HttpStatusCode.Redirect
+                        || response.StatusCode == HttpStatusCode.MovedPermanently
+                        || response.StatusCode == HttpStatusCode.TemporaryRedirect
+                       )
+                    {
+                        return response.Headers["Location"];
+                    }
+                }
+            }
+            catch(System.Net.WebException e)
+            {
+                if(e.Status == WebExceptionStatus.ProtocolError)
+                {
+                    var response = (HttpWebResponse)e.Response;
+                    if(response.StatusCode == HttpStatusCode.Redirect
+                       || response.StatusCode == HttpStatusCode.MovedPermanently
+                       || response.StatusCode == HttpStatusCode.TemporaryRedirect
+                      )
+                    {
+                        return response.Headers["Location"];
+                    }
+                }
+
+                // Handle other cases if needed
+            }
+
+            // No redirect, return the original url
+            return url;
         }
     }
 }
